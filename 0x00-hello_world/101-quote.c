ca0c5e08fee983e3e2d7c4415a4f56c44150c830
CHIGODI <chiegody254@gmail.com>
2023-08-10 06:49:25 -0700

    Added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ * main - Entry point
+ * Description:  program that prints a provided string exactly as it is
+ *
+ *Return: Always 1 (error)
+ *
+ */
+int main(void)
+{
+  write(2,"and that piece of art is useful\"\ - Dora Korpar, 2015-10-19",strlen("and that piece of art is useful\"\ - Dora Korpar, 2015-10-19");
+  return 1;
+}
