3c7c0a60613ec096428df964f67768a88e356b9e
CHIGODI <chiegody254@gmail.com>
2023-08-10 04:54:32 -0700

    Added 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point for the program
+ *
+ *description: This is the main function that prints
+ *with proper grammar, but the outcome is a piece of art,
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return (0);
+}
