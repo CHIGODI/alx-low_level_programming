688fb0184dedd29419d1cc0663745f7f21847b21
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-10 16:51:46 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,14 @@
 
 /**
  * main - Entry point
+ *
  * Description: A program that prints a provided string exactly as it is.
+ *
  * Return: Always 1 (error)
  */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+          strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
     return (1);
 }
