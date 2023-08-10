6e1d140e16697c88282e0dbbc128f3947e42a6fa
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-10 13:34:15 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 *program to print a string with put function
 *Return: Always 0 (Success)
 */
-int main(void)
+void main()
 {   
  puts("\"Programming is like building a multilingual puzzle");
  return (0);
