ebab4507e701499baea7170d46147695d66ac628
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-10 16:50:24 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
 /**
- *
  * main - Entry point
- * Description:  program that prints a provided string exactly as it is
- *
- *Return: Always 1 (error)
- *
+ * Description: A program that prints a provided string exactly as it is.
+ * Return: Always 1 (error)
  */
 int main(void)
 {
-  write(2,"and that piece of art is useful\"\ - Dora Korpar, 2015-10-19",strlen("and that piece of art is useful\"\ - Dora Korpar, 2015-10-19");
-  return 1;
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+    return (1);
 }
