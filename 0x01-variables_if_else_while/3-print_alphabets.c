34371eda08b59e517c3d9b62b3efd8eea767c80f
CHIGODI <chiegody254@gmail.com>
2023-08-11 05:22:15 -0700

    Added 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase and uppercase,
+ *         followed by a new line using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[26] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z'
+	};
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabets[i]);
+		putchar(toupper(alphabets[i]));
+	}
+	putchar('\n');
+
+	return (0);
+}
