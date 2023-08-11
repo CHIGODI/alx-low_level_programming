95e136b030ed4c659f2aa21c95152e44062299f3
CHIGODI <chiegody254@gmail.com>
2023-08-11 06:07:50 -0700

    Added 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all alphabets except e and q.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar(\n);
+	return (0);
+}
