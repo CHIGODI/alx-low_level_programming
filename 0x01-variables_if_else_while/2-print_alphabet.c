d90bb0bd4b47eaeba5ab779581d11236f58d2c87
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-11 15:07:57 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,17 +8,18 @@
  */
 int main(void)
 {
-	char alphabets[26] = {
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-		'u', 'v', 'w', 'x', 'y', 'z'
-	};
-	int i;
+    char alphabets[26] = {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'
+    };
+    int i;
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alphabets[i]);
-	}
+    for (i = 0; i < 26; i++)
+    {
+        putchar(alphabets[i]);
+    }
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
