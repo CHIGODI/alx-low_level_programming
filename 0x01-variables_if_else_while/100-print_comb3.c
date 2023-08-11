4d0eff81998bf7cf764bcf0b0a484daa895811bc
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-11 22:32:12 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - A program to print combination of numbers using putchar.
+ * main - A program to print unique combinations of numbers using putchar.
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
-	int numbers;
+   	 int first, second;
 
-	for (numbers = 0; numbers <= 89; numbers++)
-	{
-		int first = numbers / 10;
-		int last = numbers % 10;
+    	for (first = 0; first <= 8; first++)
+    	{
+       		 for (second = first + 1; second <= 9; second++)
+       	 {
+          	  putchar(first + '0');
+            	putchar(second + '0');
+	            if (first < 8 || second < 9)
+            	{
+                	putchar(',');
+                	putchar(' ');
+           	 }
+        	}
+   	 }
 
-		if (first != last && (first * 10 + last) != (last * 10 + first))
-		{
-			putchar(first + '0');
-			putchar(last + '0');
-		}
-		if (numbers < 89 && first != last)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+    putchar('\n');
+    return (0);
 }
