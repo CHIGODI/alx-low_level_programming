74349dec8451da7d05827c8bf0a9ed8d76e5483b
CHIGODI <chiegody254@gmail.com>
2023-08-11 05:00:39 -0700

    Added 2-prints_akphabets.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lowercase, followed by a new
+ *        line using putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[26] = {
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z'
+	};
+
+	for (int i = 0; i < sizeof(alphabets); i++)
+	{
+		putchar(alphabets[i]);
+		putchar('\n');
+	}
+
+	return (0);
+}
