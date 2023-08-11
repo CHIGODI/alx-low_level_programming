d2011dd6c1a3542b41118810d8f6fc0865b66dc5
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-11 15:04:45 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,31 +15,7 @@ int main(void)
     };
     int i;
 
-    for (i = 0; i < sizeof(alphabets); i++)
-    {
-        putchar(alphabets[i]);
-        putchar('\n');
-    }
-
-    return (0);
-}
-#include <stdio.h>
-/**
- * main - A program that prints the alphabet in lowercase, followed by a new
- *        line using putchar.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    char alphabets[26] = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y', 'z'
-    };
-    int i;
-
-    for (i = 0; i < sizeof(alphabets); i++)
+    for (i = 0; i < 26; i++)
     {
         putchar(alphabets[i]);
         putchar('\n');
