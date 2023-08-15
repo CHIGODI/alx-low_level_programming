32ba1701fa58c533f2eebfaee7d55cd764ddca1d
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-16 00:06:46 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,9 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		last_digit = -last_digit;
 	}
-	putchar(last_digit + '0');
+	_putchar(last_digit + '0');
 	return (0);
 }
