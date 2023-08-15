44ce3d59001d60e696f2a6ac799b7238178423ae
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-15 18:28:01 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - A function that checks for lowercase characters.
  *
- * parameter c - parameter c is the character.
+ * c - parameter c is the character.
  *
  * Return: 1 is lower 0 otherwise.
  *
