a54e3e15ea9672c66db57e452ebb0b37324d051c
CHIGODI <chiegody254@gmail.com>
2023-08-15 21:27:42 +0300

    Added 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _isalpha - Fuction that checks for alphabet character.
+ *
+ *int c - is the parameter for the function.
+ *
+ * Return: 1 is letter 0 otherwise.
+ *
+ */
+int _isalpha(int c)
+{
+	char l;
+	int flag = 0;
+
+	for (l = 'a', k = 'A'; l <= 'z', k <= 'Z'; l++, k++)
+	{
+		if (c == l || c == k)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 1)
+		return (1);
+	return (0);
+}
