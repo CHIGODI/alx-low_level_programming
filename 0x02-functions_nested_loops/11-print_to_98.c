09332c6c869af678b6ee9f338039a6693b3e5864
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-16 01:24:44 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,35 +8,43 @@
  */
 void print_to_98(int n)
 {
-        int i, j;
+    int i, j;
 
-        if (n < 98)
+    if (n < 98)
+    {
+        for (i = n; i <= 98; i++)
         {
-            for (i = n; i <= 98; i++)
+            if (n < 0)
             {
                 i = -i;
                 _putchar('-');
                 _putchar((i / 10) + '0');
                 _putchar((i % 10) + '0');
-                if (i != 98)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
             }
-        }
-        else
-        {
-            for (j = n; j >= 98; j--)
+            else
             {
-                _putchar((j / 10) + '0');
-                _putchar((j % 10) + '0');
-                if (j != 98)
-                {
-                    _putchar(',');
-                    _putchar(' ');
-                }
+                _putchar((i / 10) + '0');
+                _putchar((i % 10) + '0');
+            }
+            if (i != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
             }
         }
-        _putchar('\n');
+    }
+    else
+    {
+        for (j = n; j >= 98; j--)
+        {
+            _putchar((j / 10) + '0');
+            _putchar((j % 10) + '0');
+            if (j != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
         }
+    }
+    _putchar('\n');
+}
