aefd1c8a86679a85e782149443124b1fa1298249
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-15 23:37:32 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,31 +8,31 @@
  */
 void print_to_98(int n)
 {
-    int i, j;
+        int i, j;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
+        if (n <= 98)
         {
-            _putchar(i + '0');
-            if (i != 98)
+            for (i = n; i <= 98; i++)
             {
-                _putchar(',');
-                _putchar(' ');
+                _putchar(i + '0');
+                if (i != 98)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                }
             }
         }
-    }
-    else
-    {
-        for (j = n; j >= 98; j--)
+        else
         {
-            _putchar(j + '0');
-            if (j != 98)
+            for (j = n; j >= 98; j--)
             {
-                _putchar(',');
-                _putchar(' ');
+                _putchar(j + '0');
+                if (j != 98)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                }
             }
         }
-    }
-    _putchar('\n'); // Print a new line
-}
+        _putchar('\n');
+        }
