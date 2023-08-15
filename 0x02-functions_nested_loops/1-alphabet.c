93744b5034fc4c6c31cc4067bda7fa1cad797ccc
CHIGODI <chiegody254@gmail.com>
2023-08-15 17:00:05 +0300

    Crrected 1-alphabet to only contain its function defination

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,5 @@
-#include <stdio.h>
 /**
- * main - A program to print alphabets in lowercase using putchar function.
- *
- * Return: Always 0(Success)
- *
- */
-void print_alphabet_x10(void);
-int main(void)
-{
-	print_alphabet_x10();
-	putchar('\n');
-	return (0);
-}
-/**
- * print_alphabet_x10 - A function to loop over alphabets.
+ * print_alphabet - A function to loop over alphabets.
  *
  */
 void print_alphabet_x10(void)
