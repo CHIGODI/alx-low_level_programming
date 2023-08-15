77a6c3136818c47dfbd049b8b568621da7004678
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-16 01:26:32 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,24 +8,25 @@
  */
 void print_to_98(int n)
 {
-    int i, j;
+    int i;
 
-    if (n < 98)
+    if (n <= 98)
     {
         for (i = n; i <= 98; i++)
         {
-            if (n < 0)
+            if (i < 0)
             {
-                i = -i;
+                int abs_i = -i; // Store the absolute value for printing
                 _putchar('-');
-                _putchar((i / 10) + '0');
-                _putchar((i % 10) + '0');
+                _putchar((abs_i / 10) + '0');
+                _putchar((abs_i % 10) + '0');
             }
             else
             {
                 _putchar((i / 10) + '0');
                 _putchar((i % 10) + '0');
             }
+
             if (i != 98)
             {
                 _putchar(',');
@@ -35,11 +36,12 @@ void print_to_98(int n)
     }
     else
     {
-        for (j = n; j >= 98; j--)
+        for (i = n; i >= 98; i--)
         {
-            _putchar((j / 10) + '0');
-            _putchar((j % 10) + '0');
-            if (j != 98)
+            _putchar((i / 10) + '0');
+            _putchar((i % 10) + '0');
+
+            if (i != 98)
             {
                 _putchar(',');
                 _putchar(' ');
