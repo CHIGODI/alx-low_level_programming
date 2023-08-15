4491154e77d0799f67196dbf247d0f60aaa0afd2
CHIGODI <chiegody254@gmail.com>
2023-08-15 17:08:50 +0300

    Updated 1-alphabets.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,5 @@
 /**
-<<<<<<< HEAD
- * print_alphabet - A function to loop over alphabets.
-=======
- * main - A program to print alphabets in lowercase using putchar function.
- *
- * Return: Always 0(Success)
- *
- */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	putchar('\n');
-	return (0);
-}
-/**
  * print_alphabet_x10 - A function to loop over alphabets.
->>>>>>> 0917e52f76fc027005ec24cbad0be10aa0164e89
  *
  */
 void print_alphabet(void)
