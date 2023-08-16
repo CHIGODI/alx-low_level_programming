dba479412f0a72b6840bea2eeb29ef8131a81405
CHIGODI <chiegody254@gmail.com>
2023-08-16 09:22:33 +0300

    corrected whole of file 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,46 +7,41 @@
  */
 void print_to_98(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            if (i < 0)
-            {
-		    putchar(' ');
-                }
-	    int abs_i = -i;
-	    _putchar('-');
-	    _putchar((abs_i / 10) + '0');
-	    _putchar((abs_i % 10) + '0');
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i < 0)
+			{
+				_putchar('-');
+				_putchar((i * -1) / 10 + '0');
+				_putchar((i * -1) % 10 + '0');
+			} else
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
 	}
 	else
 	{
-                _putchar((i / 10) + '0');
-                _putchar((i % 10) + '0');
-            }
-
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-    }
-}
-else
-{
-        for (i = n; i >= 98; i--)
-        {
-		_putchar((i / 10) + '0');
-		_putchar((i % 10) + '0');
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    _putchar('\n');
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
