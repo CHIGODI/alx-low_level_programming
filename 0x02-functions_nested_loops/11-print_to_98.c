91d23f25dbac1c976ea03cad754c3f97e6db15c4
CHIGODI <chiegody254@gmail.com>
2023-08-16 13:20:09 +0300

    updated 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
  * print_to_98 - Function that prints all natural numbers from n to 98.
  * @n: number that will start.
@@ -8,65 +8,20 @@
  */
 void print_to_98(int n)
 {
-    int i;
-    int abs_num, divisor;
+	if (n <= 98)
+	{
+		int num;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            if (i < 0)
-            {
-                _putchar('-');
-                abs_num = -i;
-                divisor = 1;
-                while (abs_num / divisor > 9)
-                    divisor *= 10;
-                while (divisor > 0)
-                {
-                    _putchar((abs_num / divisor) + '0');
-                    abs_num %= divisor;
-                    divisor /= 10;
-                }
-            }
-            else
-            {
-                divisor = 1;
-                while (i / divisor > 9)
-                    divisor *= 10;
-                while (divisor > 0)
-                {
-                    _putchar((i / divisor) + '0');
-                    i %= divisor;
-                    divisor /= 10;
-                }
-            }
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            divisor = 1;
-            while (i / divisor > 9)
-                divisor *= 10;
-            while (divisor > 0)
-            {
-                _putchar((i / divisor) + '0');
-                i %= divisor;
-                divisor /= 10;
-            }
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    _putchar('\n');
+		for (num = n; num <= 98; num++)
+		{
+			printf("%d", num);
+		}
+	}
+	else if (num > 98)
+	{
+		for (num = n; n >= 98; num++)
+		{
+			printf("%d", num);
+		}
+	}
 }
