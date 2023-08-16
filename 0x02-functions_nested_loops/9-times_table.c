e059d7ae2f01317e0032be98985a3cdf3b71f990
CHIGODI <chiegody254@gmail.com>
2023-08-16 12:24:46 +0300

    Made 9-times_table.c betty compliant

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,32 @@
 #include "main.h"
-
 /**
  * times_table - function that prints 9 times table starting with 0.
  */
 void times_table(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            int times = i * j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int times = i * j;
 
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-
-                if (times < 10)
-                    _putchar(' ');
-                else
-                    _putchar(times / 10 + '0');
-
-                _putchar(times % 10 + '0');
-            }
-            else
-            {
-                _putchar('0');
-            }
-        }
-        _putchar('\n');
-    }
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (times < 10)
+					_putchar(' ');
+				else
+					_putchar(times / 10 + '0');
+				_putchar(times % 10 + '0');
+			}
+			else
+			{
+				_putchar('0');
+			}
+		}
+		_putchar('\n');
+	}
 }
