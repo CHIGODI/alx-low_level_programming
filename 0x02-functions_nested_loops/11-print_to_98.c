8d1eea2642a3057977a979b964d91450c9640fff
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-16 12:29:08 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_to_98 - Function that prints all natural numbers from n to 98.
  * @n: number that will start.
@@ -7,41 +8,64 @@
  */
 void print_to_98(int n)
 {
-	int i;
+    int i;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i < 0)
-			{
-				_putchar('-');
-				_putchar((i * -1) / 10 + '0');
-				_putchar((i * -1) % 10 + '0');
-			} else
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-			}
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	_putchar('\n');
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            if (i < 0)
+            {
+                _putchar('-');
+                int abs_num = -i;
+                int divisor = 1;
+                while (abs_num / divisor > 9)
+                    divisor *= 10;
+                while (divisor > 0)
+                {
+                    _putchar((abs_num / divisor) + '0');
+                    abs_num %= divisor;
+                    divisor /= 10;
+                }
+            }
+            else
+            {
+                int divisor = 1;
+                while (i / divisor > 9)
+                    divisor *= 10;
+                while (divisor > 0)
+                {
+                    _putchar((i / divisor) + '0');
+                    i %= divisor;
+                    divisor /= 10;
+                }
+            }
+            if (i != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
+        {
+            int divisor = 1;
+            while (i / divisor > 9)
+                divisor *= 10;
+            while (divisor > 0)
+            {
+                _putchar((i / divisor) + '0');
+                i %= divisor;
+                divisor /= 10;
+            }
+            if (i != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    _putchar('\n');
 }
