7a1420ce2b4d84f6b11e59cb754408b07570793b
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-16 13:45:05 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-		for (num = n; num >= 98; num++)
+		for (num = n; num >= 98; num--)
 		{
 			printf("%d", num);
 			if (num < 98)
@@ -32,4 +32,5 @@ void print_to_98(int n)
 			}
 		}
 	}
+	printf("\n");
 }
