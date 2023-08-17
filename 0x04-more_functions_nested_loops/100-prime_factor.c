73c6a4a7eda5f46f31fe5b5e2ac27c9c5612021a
CHIGODI <chiegody254@gmail.com>
2023-08-17 16:27:00 +0300

    Added 100-prime_factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+nclude <stdio.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Description: Finds and prints the largest prime factor of a number.
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long long int num = 612852475143;
+	long long int largest_PrimeFactor = 2;
+
+	while (num > largest_PrimeFactor)
+	{
+		if (num % largest_PrimeFactor == 0)
+		{
+			num /= largest_PrimeFactor;
+		}
+		else
+		{
+			largest_PrimeFactor++;
+		}
+	}
+	printf("%lld\n", largest_PrimeFactor);
+	return 0;
+}
