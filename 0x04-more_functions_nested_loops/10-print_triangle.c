73714fc7aeacb02398be1613c525c54f577ca87b
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-17 14:36:11 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_triangle - function that prints a triangle
+ * print_triangle - a function that prints a triangle
  * @size: is the size of the triangle
  *
  * Return: void
@@ -9,7 +9,6 @@
 void print_triangle(int size)
 {
 	int i, j;
-
 
 	if (size <= 0)
 	{
