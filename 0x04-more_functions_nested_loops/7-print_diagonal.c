5504c871ce7ffb29966773427c25873dba1737a4
CHIGODI <chiegody254@gmail.com>
2023-08-17 22:07:30 +0300

    Updated 7-print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,21 +14,12 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < i; j++)
 			{
-				if (j == i)
-				{
-					_putchar('\\');
-				}
-				else
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
-			if (i != n - 1)
-            		{
-                	_putchar('\n');
-            		}
+		     	_putchar('\\');
+			_putchar('\n');
 		}
 	}
 }
