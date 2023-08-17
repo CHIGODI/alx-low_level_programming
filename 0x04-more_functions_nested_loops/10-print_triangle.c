ffce2863371b9001afba7320187b067fd1392dc5
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-17 13:47:04 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,15 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < size; j++)
 		{
-			if (j < (n - 1 - i))
+			if (j < (size - 1 - i))
 			{
 				_putchar(' ');
 			}
-			else if (j >= (n - 1 - i))
+			else if (j >= (size - 1 - i))
 			{
 				_putchar('#');
 			}
