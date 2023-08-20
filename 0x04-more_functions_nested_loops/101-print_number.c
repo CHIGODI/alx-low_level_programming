e5ba0ef7fd2c2b75b1321f6e40954994198655e2
CHIGODI <chiegody254@gmail.com>
2023-08-20 15:40:31 +0300

    Updated 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "main.h"
+111111111111111111111111111111111111#include "main.h"
 
 /**
  * print_number - function that prints integer
@@ -6,15 +6,17 @@
  *
  * Return: void
  */
-void print_number(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    
-    if (n >= 10) {
-        print_number(n / 10);
-    }
-    
-    _putchar(n % 10 + '0');
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
 }
