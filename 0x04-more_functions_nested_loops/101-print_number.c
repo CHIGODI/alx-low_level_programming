2e0e9fc42ef2cb1abd6a73a76d63e22152f4e94a
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-20 15:34:16 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,19 +6,15 @@
  *
  * Return: void
  */
-void print_number(int n)
-{
-	
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		print_number(n / 10 + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n > 0)
-	{
-		print_number(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
+void print_number(int n) {
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
+    
+    if (n >= 10) {
+        print_number(n / 10);
+    }
+    
+    putchar(n % 10 + '0');
 }
