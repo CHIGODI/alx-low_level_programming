aa25902b4343bd71ee4124b33be4a17696c0bae0
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-08-21 21:59:52 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -24,14 +24,14 @@ int main(void)
     char password[PASSWORD_LENGTH + 1];
     int i;
 
-    srand(time(NULL)); // Seed the random number generator
+    srand(time(NULL)); /* Seed the random number generator */
 
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
         password[i] = generateRandomCharacter();
     }
 
-    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password string
+    password[PASSWORD_LENGTH] = '\0'; /* Null-terminate the password string */
 
     printf("Generated password: %s\n", password);
 
