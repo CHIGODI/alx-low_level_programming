f4723cda9a7d4693df3d1652243f4aaa64427bfa
CHIGODI <chiegody254@gmail.com>
2023-08-21 14:46:03 +0300

    Added defination for prototype print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string in reverse
+ *            followd by a new line.
+ * @s: string to be reversed
+ *
+ * Return: void
+ *
+ */
+void print_rev(char *s)
+{
+	int len = 0, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
