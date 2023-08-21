3c0cccdb4c9dfffc111774beebc540d0e0a3773a
CHIGODI <chiegody254@gmail.com>
2023-08-21 16:09:30 +0300

    Added function defination for prototype puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 -a function that prints every other character of a string
+ *         starting with the first character followed by new line
+ *
+ * @str: string to be used
+ *
+ * Return: void
+ *
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
