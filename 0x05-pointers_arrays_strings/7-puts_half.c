813cf155a1ea8188fcc5c5fb3630fd6945b8ed83
CHIGODI <chiegody254@gmail.com>
2023-08-21 17:09:23 +0300

    Added function defination for puts_half prototype

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * puts_half - function that prints half a string, followed by newline
+ *           -should print secon half
+ *           - if number of characters is odd print last n characters
+ *           - n = (length_of_the_string - 1)/2
+ * @str: string to be used
+ *
+ * Return: void
+ *
+ */
+void puts_half(char *str)
+{
+	int i, n, length_of_the_string;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length_of_the_string++;
+	}
+
+	if (length_of_the_string % 2 != 0)
+	{
+		n = (length_of_the_string - 1) / 2;
+
+		for (i = n; i < length_of_the_string; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		n = length_of_the_string / 2;
+
+		for (i = n; i < length_of_the_string; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+}
