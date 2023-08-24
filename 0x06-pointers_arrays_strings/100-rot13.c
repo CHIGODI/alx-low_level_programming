a80b5113f0e40693384d32df691e2e72210aca9d
CHIGODI <chiegody254@gmail.com>
2023-08-24 10:44:34 +0300

    Added function defination for prototype char *rot13(char *); to file 100-rot13.c!

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - a function that encodes a string using rot13
+ * @s: string to be used
+ *
+ * Return: s
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char alpha[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char rot[] =  "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
