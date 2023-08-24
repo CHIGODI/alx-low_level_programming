05f243887521d08c0910baf2873ca35e0bafa7e4
CHIGODI <chiegody254@gmail.com>
2023-08-24 10:31:28 +0300

    Updated 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,29 +8,19 @@
 
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char letter[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
+		for (j = 0; letter[j] != '\0'; j++)
 		{
-			s[i] = '4';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
-			s[i] = '1';
+			if (s[i] == letter[j])
+			{
+				s[i] = num[j];
+				break;
+			}
 		}
 	}
 	return (s);
