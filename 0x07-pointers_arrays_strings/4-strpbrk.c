7a42beaa0d2f07f0cd949fa850d9c54b09b7a95f
CHIGODI <chiegody254@gmail.com>
2023-08-28 22:17:14 +0300

    Added function defination for prototypechar *_strpbrk(char *s, char *accept);

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,17 +8,22 @@
  * Return: Pointer to the byte in s that matches one of the bytes in accept,
  *         or NULL if no such byte is found.
  */
-char *_strpbrk(char *s, char *accept) {
-    while (*s) {
-        char *a = accept;
-        while (*a) {
-            if (*s == *a) {
-                return s; 
-            }
-            a++;
-        }
-        s++;
-    }
-    
-    return NULL;
+char *_strpbrk(char *s, char *accept)
+{
+	char *a  = NULL;
+
+	while (*s)
+	{
+		a = accept;
+		while (*a)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
 }
