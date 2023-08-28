ffb8f77ae090348b3e8ee047e35872ed788be236
CHIGODI <chiegody254@gmail.com>
2023-08-28 16:48:19 +0300

    Added function defination for prototype char *_strchr(char *s, char c);

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: pointer to string to be used
+ * @c: character match to be found
+ *
+ * Return: NULL if not found. pointer to first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ptr = NULL;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			break;
+		}
+	}
+	return (ptr);
+}
