0cd814eccd09ab671d443d8e49376d45935da1d1
CHIGODI <chiegody254@gmail.com>
2023-08-28 21:38:52 +0300

    Added function defination for prototype void set_string(char **s, char *to);

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * set_string - Sets the value of a pointer to a char.
- * @s: Pointer to the pointer that needs to be set.
- * @to: Pointer to the value that 's' should point to.
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to the pointer thatneeds to be set
+ * @to: pointer to the value that S should point to
+ *
  */
-void set_string(char **s, char *to) {
-    *s = to; 
+void set_string(char **s, char *to)
+{
+	*s = to;
 }
