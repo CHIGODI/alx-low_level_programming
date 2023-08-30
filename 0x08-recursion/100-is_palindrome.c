7c27fbd1f9d852438e226b1085005c644eda850a
CHIGODI <chiegody254@gmail.com>
2023-08-30 22:41:43 +0300

    Added function defination for prototype int is_palindrome(char *s);

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * check_palindrome - helper function to check recursion
+ * @s: begining of string
+ * @end: end of string
+ *
+ * Return: 1 if true 0 otherwise
+ */
+int check_palindrome(char *s, char *end)
+{
+	if (s >= end)
+	{
+		return (1);
+	}
+
+	if (*s != *end)
+	{
+		return (0);
+	}
+	return (check_palindrome(s+1, end-1));
+}
+/**
+ * is_palindrome -a function to check if palydrome of not
+ * @s: string to be checked
+ *
+ * Return: 1 if true 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	char *end = s + strlen(s) - 1;
+
+	return (check_palindrome(s, end));
+}
