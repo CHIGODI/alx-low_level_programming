57cb8e95c3c8a885f64aecb24e840619569210ca
CHIGODI <chiegody254@gmail.com>
2023-09-01 21:32:04 +0300

    Added function main to 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * main - entry point of program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	unsigned int sum = 0, num;
+
+	if (argc < 3)
+	{
+		printf("0\n");
+		return (1);
+	}
+	else if (argc >= 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			num = atoi(argv[i]);
+			if (num > UINT_MAX)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += num;
+		}
+		printf("%d\n", sum);
+	}
+	return (1);
+}
