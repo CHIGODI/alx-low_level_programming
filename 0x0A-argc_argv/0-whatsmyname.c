2284ecaabb631ca631135ffa7deea81d8c03b6e1
CHIGODI <chiegody254@gmail.com>
2023-09-01 15:24:24 +0300

    updated 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,13 +7,13 @@
  *
  * Return: 0 success
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
