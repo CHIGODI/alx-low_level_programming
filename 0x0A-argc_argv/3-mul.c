c17226c139abc2cae473b7b99c08fc095d434b2f
CHIGODI <chiegody254@gmail.com>
2023-09-01 16:40:44 +0300

    Added function main to 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc  == 1)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+		return (0);
+	}
+}
