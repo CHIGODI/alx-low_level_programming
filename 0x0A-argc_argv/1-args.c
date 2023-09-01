35561efa1977b58928969ed3cf4f9162b1b8d9ae
CHIGODI <chiegody254@gmail.com>
2023-09-01 15:49:30 +0300

    Added function main to 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 success
+ */
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
