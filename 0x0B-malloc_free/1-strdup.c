495ad73bda42b595eae4cdb0a80f132052bc05cb
CHIGODI <chiegody254@gmail.com>
2023-09-05 13:03:32 +0300

    1-strdup.c file was added with defination of prototype char *_strdup(char *str);

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup -a function that returns a pointer to a newly
+ *                      allocated space in memory, which contains a
+ *                      copy of the string given as a parameter.
+ * @str: string to  be used
+ *
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+	return (strdup(str));
+}
