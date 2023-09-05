737ee37d4b1dd59c37c9c7410c4d7dd913ec62d3
CHIGODI <chiegody254@gmail.com>
2023-09-05 12:45:19 +0300

    added function defination of prototype char *create_array(unsigned int size, char c); to file 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - a function that creates an array of chars,
+ *                and initializes it with a specific char.
+ * @c: character initiliazedto array
+ * @size: the size
+ *
+ * Return: Nothing.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = NULL;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (char *)malloc((size + 1) * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	ptr[size] = '\0';
+
+	return (ptr);
+}
