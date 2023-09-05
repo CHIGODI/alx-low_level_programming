85674ceee6c5331ae71d40d8b6b764d921a300ef
CHIGODI <chiegody254@gmail.com>
2023-09-05 13:51:38 +0300

    Added function defination of prototype char *str_concat(char *s1, char *s2) to file 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to newly allocated space  NULL if failure .
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1 = strlen(s1);
+	int len2 = strlen(s2);
+	char *ptr = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	ptr = (char *)malloc(len1 + len2);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(ptr, s1);
+	strcat(ptr, s2);
+
+	return (ptr);
+}
