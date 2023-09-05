b0ddd07e09015a18ba1bae5e395430a1cfeb174f
CHIGODI <chiegody254@gmail.com>
2023-09-05 20:38:51 +0300

    Aded function defination to the prototype char *argstostr(int ac, char **av); to file100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all arguments of a program.
+ * @ac: Number of arguments.
+ * @av: Array of argument strings.
+ *
+ * Return: Pointer to the concatenated string or NULL on failure.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, total_len = 0;
+	char *concatenated;
+	int k = 0;
+	
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			total_len++;
+		}
+		total_len++;
+	}
+	concatenated = (char *)malloc(total_len * sizeof(char));
+	if (concatenated == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			concatenated[k++] = av[i][j];
+		}
+		concatenated[k++] = '\n';
+	}
+	concatenated[k] = '\0';
+	return (concatenated);
+}
