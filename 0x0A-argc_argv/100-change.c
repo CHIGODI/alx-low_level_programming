ba2e2c685b1fa69fb8898665957b2b95dba2d5f6
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-06 13:22:53 +0300

    Update 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+int main(int argc, char *argv[])
+{
+        int change;
+        int coins [] = {25, 10, 5, 2, 1};
+        int num_of_coins = 0;
+        int coin_count = 0;
+        int i = 0;
+
+        if (argc != 2)
+        {
+                printf("Error\n");
+                return (1);
+        }
+
+        change = atoi(argv[1]);
+
+        if (change < 0)
+        {
+                printf("0\n");
+                return (0);
+        }
+
+        while (change > 0)
+        {
+                if (change >= coins[i])
+                {
+                        num_of_coins = change / coins[i];
+                        coin_count += num_of_coins;
+                        change %= coins[i];
+                }
+                i++;
+        }
+        printf("%d\n", coin_count);
+        return (0);
+} 
