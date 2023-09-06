70be034dd6a460718cb22ab42325477f81c49d93
CHIGODI <chiegody254@gmail.com>
2023-09-06 16:51:11 +0300

    Added function defination of main to file 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,45 @@
-#include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
  * main - entry point of the program
  * @argc: argument count
  * @argv: argument vector
  *
  * Return: 0 success
- */int main(int argc, char *argv[])
+ */
+int main(int argc, char *argv[])
 {
-        int change;
-        int coins [] = {25, 10, 5, 2, 1};
-        int num_of_coins = 0;
-        int coin_count = 0;
-        int i = 0;
+	int change;
+	int coins [] = {25, 10, 5, 2, 1};
+	int num_of_coins = 0;
+	int coin_count = 0;
+	int i = 0;
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-        change = atoi(argv[1]);
+	change = atoi(argv[1]);
 
-        if (change < 0)
-        {
-                printf("0\n");
-                return (0);
-        }
+	if (change < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-        while (change > 0)
-        {
-                if (change >= coins[i])
-                {
-                        num_of_coins = change / coins[i];
-                        coin_count += num_of_coins;
-                        change %= coins[i];
-                }
-                i++;
-        }
-        printf("%d\n", coin_count);
-        return (0);
+	while (change > 0)
+	{
+		if (change >= coins[i])
+		{
+			num_of_coins = change / coins[i];
+			coin_count += num_of_coins;
+			change %= coins[i];
+		}
+		i++;
+	}
+	printf("%d\n", coin_count);
+	return (0);
 }
