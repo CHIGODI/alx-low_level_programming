689c5279fc9dbffa468b0126670f1b2a5b72e1b5
CHIGODI <chiegody254@gmail.com>
2023-09-06 16:36:22 +0300

    Completely changed file 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,30 +10,29 @@
  */
 int main(int argc, char *argv[])
 {
-        int sum = 0;
-        int i, n, j;
+	int sum = 0;
+	int i, n, j;
 
-        if (argc < 2)
-        {
-                printf("0\n");
-        }
-        else
-        {
-                for (i = 1; i < argc; i++)
-                {
-                        for (j = 0; argv[i][j] != '\0'; j++)
-                        {
-                                if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-                                {
-                                        printf("Error\n");
-                                        return (1);
-                                }
-                                n = atoi(argv[i]);
-                        }
-
-                        sum += n;
-                }
-                printf("%d\n", sum);
-        }
-                return (0);
-} 
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				n = atoi(argv[i]);
+			}
+			sum += n;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
