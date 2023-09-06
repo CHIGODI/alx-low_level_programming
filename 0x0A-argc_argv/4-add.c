14466d0ac6466d059d5f5435dba8a8adaccb0e26
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-06 13:52:30 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -28,7 +28,6 @@ int main(int argc, char *argv[])
                                         printf("Error\n");
                                         return (1);
                                 }
-                                /* takes care of positive numbers */
                                 n = atoi(argv[i]);
                         }
 
