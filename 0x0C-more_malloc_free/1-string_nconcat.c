b7da00cf03f4a686bf6710bbe6604d84e60fc10a
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-08 00:04:19 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -96,9 +96,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len2 = _strlen(s2);
 
 	if (n >= len2)
-		n = len2 + 1;
-
-	concat = malloc(len1 + n + 1);
+		concat = malloc(sizeof(char) * len1 + len2 + 1);
+	concat = malloc(sizeof(char) * len1 + n + 1);
 
 	if (concat == NULL)
 	{
