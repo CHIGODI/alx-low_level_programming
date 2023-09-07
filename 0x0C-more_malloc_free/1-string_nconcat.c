3aca1b1e597aa65ab7abbb907573270228fb0b2b
CHIGODI <chiegody254@gmail.com>
2023-09-07 13:09:40 +0300

    Optimised the function for prototype char *string_nconcat(char *s1, char *s2, unsigned int n);

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -89,28 +89,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	if (n <= len2)
+
+	if (n >= len2)
+		n = len2;
+
+	concat = malloc(len1 + n + 1);
+
+	if (concat == NULL)
 	{
-		concat = malloc(len1 + len2 + 1);
-		if (concat == NULL)
-		{
-			return (NULL);
-		}
-		_strcpy(concat, s1);
-		_strncat(concat, s2, n);
-		return (concat);
+		return (NULL);
 	}
-	else
-	{
-		concat = malloc(len1 + n + 1);
-		if (concat == NULL)
-		{
-			return (NULL);
-		}
-		_strcpy(concat, s1);
-		_strncat(concat, s2, n);
-		return (concat);
-	}
+
+	_strcpy(concat, s1);
+	_strncat(concat, s2, n);
+
+	return (concat);
 }
