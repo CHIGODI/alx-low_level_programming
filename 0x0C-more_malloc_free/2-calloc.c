3f15b4fec09e626f0058510ea5e2111c114656b4
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-07 14:16:02 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,17 +9,11 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *memory = malloc(nmemb * size);
-	unsigned int size_t = size * nmemb;
+	unsigned int *memory = calloc(nmemb * size);
 	unsigned int i;
 
 	if (memory == NULL)
 		return (NULL);
-
-	for (i = 0; i < size_t; i++)
-	{
-		memory[i] = 0;
-	}
 
 	return (memory);
 }
