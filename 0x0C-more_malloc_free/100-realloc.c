5611ddb4482b43a4c3fb3fb91d8e05b9d3d3e8d2
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-07 22:10:23 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -60,6 +60,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		}
 		_memcpy(new_pointer, ptr, new_size);
+		return (new_pointer);
 		free(ptr);
 	}
 }
