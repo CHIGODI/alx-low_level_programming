ad10b3713013b37107aeac9c8d0e2c0aabf2ff61
CHIGODI <chiegody254@gmail.com>
2023-09-07 14:46:45 +0300

    Aded function defination for prototype int *array_range(int min, int max); to file 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - a function that creates an array of integers
+ * @min: start
+ * @max: end
+ *
+ * Return: pointer to new array NULL if failed
+ */
+int *array_range(int min, int max)
+{
+	int i;
+	int *array_mem = NULL;
+	int count = 0;
+
+	if (min > max)
+		return (NULL);
+
+	count = max - min + 1;
+
+	array_mem = malloc(count * sizeof(int));
+
+	if (array_mem == NULL)
+		return (NULL);
+	for (i = min; i <= max; i++)
+	{
+		array_mem[i] = i;
+	}
+	return (array_mem);
+}
