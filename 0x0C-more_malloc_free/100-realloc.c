60963826911e9261af15f61b47cfbe4683726fe0
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-07 22:38:13 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,71 +1,69 @@
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * _memcpy - a function that copies memory data
- * @dest: destination to copy to
- * @src: source string
- * @n: size
+ * _memcpy - A function that copies memory data
+ * @dest: Destination to copy to
+ * @src: Source string
+ * @n: Size
  *
  * Return: dest
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+void *_memcpy(void *dest, const void *src, unsigned int n)
 {
-	char *s = (char *)src;
-	char *d = (char *)dest;
-	size_t i;
+    char *s = (char *)src;
+    char *d = (char *)dest;
 
-	for (i = 0; i < n; i++)
-	{
-		d[i] = s[i];
-	}
+    for (unsigned int i = 0; i < n; i++)
+    {
+        d[i] = s[i];
+    }
 
-	return (dest);
+    return dest;
 }
+
 /**
- * _realloc - a function that reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previously allocated with malloc
- * @old_size: size of the allocated space for ptr
- * @new_size: new size of the memory block
+ * _realloc - A function that reallocates a memory block using malloc and free
+ * @ptr: A pointer to the memory previously allocated with a call to malloc
+ * @old_size: The size, in bytes, of the allocated space for ptr
+ * @new_size: The new size, in bytes, of the new memory block
  *
- * Return: pointer to reallocated memory
+ * Return: A pointer to reallocated memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_pointer;
-
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
-
-	new_pointer = malloc(new_size);
-
-	if (new_pointer == NULL)
-	{
-		return (NULL);
-	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
-	if (new_size < old_size)
-	{
-		_memcpy(new_pointer, ptr, new_size);
-	}
-	else
-	{
-		_memcpy(new_pointer, ptr, old_size);
-	}
-
-	free(ptr);
-	if (new_pointer == NULL)
-		return (NULL);
-	return (new_pointer);
+    if (new_size == 0)
+    {
+        free(ptr);
+        return NULL;
+    }
+    else if (ptr == NULL)
+    {
+        return malloc(new_size);
+    }
+    else if (new_size == old_size)
+    {
+        return ptr;
+    }
+    else if (new_size < old_size)
+    {
+        void *new_ptr = malloc(new_size);
+        if (new_ptr == NULL)
+        {
+            return NULL;
+        }
+        _memcpy(new_ptr, ptr, new_size);
+        free(ptr);
+        return new_ptr;
+    }
+    else
+    {
+        void *new_ptr = malloc(new_size);
+        if (new_ptr == NULL)
+        {
+            return NULL;
+        }
+        _memcpy(new_ptr, ptr, old_size);
+        free(ptr);
+        return new_ptr;
+    }
 }
