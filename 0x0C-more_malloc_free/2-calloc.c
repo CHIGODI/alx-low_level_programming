8509e2c1ff2e9ce3f89b6bf0b85679f085d9549b
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-07 16:04:38 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _memset - a function that fills memory with a constant byte
+ * @s: pointer to string
+ * @b: a constant to be printed
+ * @n: size
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
+/**
  * _calloc - a function that allocates memory for an array using malloc.
  * @nmemb: number of elements
  * @size: size of each element
@@ -9,17 +28,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int *memory = malloc(nmemb * size);
+	unsigned int size_t = nmemb * size;
+	char *memory = malloc(size_t);
 	unsigned int i = 0;
 
 	if (memory == NULL)
 	{
 		return (NULL);
 	}
-	while (i < nmemb)
-	{
-		memory[i] = 0;
-		i++;
-	}
+
+	_memset(memory, 0, size_t);
 	return (memory);
 }
