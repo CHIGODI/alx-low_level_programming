387775a1d352ebd850531873be3bef5f7b0fd975
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-11 15:08:59 +0300

    Update 4-new_dog.c by removing free nweDog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -55,16 +55,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	newDog->name = _strdup(name);
 	newDog->owner = _strdup(owner);
-
-
-	if (newDog->name == NULL || newDog->owner == NULL)
-	{
-		free(newDog->name);
-		free(newDog->owner);
-		free(newDog);
-		return (NULL);
-	}
-
 	newDog->age = age;
+	
 	return (newDog);
 }
