2b99c1b0697084cb545b0950ac8552777703800e
CHIGODI <chiegody254@gmail.com>
2023-09-11 13:04:07 +0300

    Updated dog.t to iclude anither dog_t  struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
