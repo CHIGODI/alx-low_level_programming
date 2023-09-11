b3927399b7a05ef75efdef2b9ba2a4e1f89f5602
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-11 15:33:08 +0300

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,12 +7,11 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d != NULL)
-	{
-		if (d->name != NULL)
-			free(d->name);
-		if (d->owner != NULL)
-			free(d->owner);
-		free(d);
-	}
+	if (d == 0)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
 }
