756e63fbb63128f5f11ad6ec378f6d8e47301b2d
CHIGODI <chiegody254@gmail.com>
2023-09-11 13:08:32 +0300

    Made dog.h betty compliant

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG
 #define DOG
 #define NULL ((void *)0)
+/**
+ * struct dog - Defines a structure representing information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog
 {
 	char *name;
@@ -8,12 +14,18 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - Defines a structure representing information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 typedef struct dog_t
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
