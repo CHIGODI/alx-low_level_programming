9c003bb5a6643f58f01006357c0816eef6bf9d18
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-13 21:44:26 +0300

    Cheek NULL for get_op_func 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -31,14 +31,15 @@ int main(int ac, char *av[])
 		exit(99);
 	}
 
-	if ((*op == '/' || *op == '%') && num2 == 0)
+	if (get_op_func(op) == NULL)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-
-	result = get_op_func(op)(num1, num2);
-
+	else
+	{
+		result = get_op_func(op)(num1, num2);
+	}
 	printf("%d\n", result);
 	return (0);
 }
