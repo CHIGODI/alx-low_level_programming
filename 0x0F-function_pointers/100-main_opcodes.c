4624c5d755a910744394fc3dc8b0c0c48592dfdf
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-09-13 23:03:58 +0300

    Add main definition 100-main_opcodes.c
    Add answer to task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print own opcode
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+    int i;
+    unsigned char *ptr = (unsigned char *)main;
+    int p;
+
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    p = atoi(argv[1]);
+
+    if (p < 0)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    for (i = 0; i < p; i++)
+    {
+        printf("%02x ", ptr[i]);
+    }
+
+    printf("\n");
+    return (0);
+}
