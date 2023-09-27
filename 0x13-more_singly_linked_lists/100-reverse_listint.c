1087515bdcf5d99c5122cdf56f0830f966735e20
CHIGODI <chiegody254@gmail.com>
2023-09-27 16:01:42 +0300

    Remove trailing white space

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,10 +10,10 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL, *temp;
 
-	if (*head == NULL) 
+	if (*head == NULL)
 		return (NULL);
 
-	while ((*head)->next != NULL) 
+	while ((*head)->next != NULL)
 	{
 		temp = (*head)->next;
 		(*head)->next = prev;
