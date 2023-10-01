2bbbddd3d8501478e1d1f6903a6e89c6e8471668
CHIGODI <chiegody254@gmail.com>
2023-10-01 12:06:10 +0300

    Fix betty errors

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,11 +2,12 @@
 /**
  * get_endianness - get s endianness
  *
- * Return: 1 if big indiannes 0 otherwise
+ * Return: 0 if big indiannes 1 otherwise
  */
-int get_endianness(void) {
-    unsigned int test = 1;
-    char *byte = (char *)&test;
+int get_endianness(void)
+{
+	unsigned int test = 1;
+	char *byte = (char *)&test;
 
-    return (int)(*byte);
+	return ((int)(*byte));
 }
