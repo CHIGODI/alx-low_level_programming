0bc2fa31bfb4db0b02e8247e0c8d1cf45b31eb59
Antony Aboud Chigodi <Chiegody254@gmail.com>
2023-10-20 13:28:04 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define BUFFER_SIZE 1024
 
+/* prototypes */
+void open_error(int exit_code, char *message, char *filename);
+void close_error(int exit_code, char *message, int fd_value);
+
 /**
- * main - Entry point for the cp program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- *
- * This program copies the content of one file to another file.
- *
- * Return: 0 on success, or an error code on failure.
+ * open_error - prints error message when opening file fails
+ * @exit_code: return code
+ * @message: error message
+ * @filename: name of file that failed to open
+ * Return: nothing
  */
-int main(int argc, char *argv[])
+void open_error(int exit_code, char *message, char *filename)
 {
-	FILE *fp_from, *fp_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+	exit(exit_code);
+}
+
+/**
+ * close_error - prints close error to stderr
+ * @exit_code: return code
+ * @message: error message
+ * @fd_value: file descriptor return value
+ * Return: nothing
+ */
+void close_error(int exit_code, char *message, int fd_value)
+{
+	dprintf(STDERR_FILENO, "%s %d\n", message, fd_value);
+	exit(exit_code);
+}
+
+/**
+ * main - main program entry.
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int fd_to = 0, fd_from = 0, bytes_read = 0, f_close = 0, total_bytes = 0;
+	char *buffer = NULL;
 
 	if (argc != 3)
 	{
@@ -26,40 +52,41 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fp_from = fopen(argv[1], "r");
-	if (fp_from == NULL)
+	buffer = malloc(BUFFER_SIZE * sizeof(char));
+	/* open source file */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from < 0)
+		open_error(98, "Error: Can't read from file", argv[1]);
+
+	/* open dest file */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to < 0)
+		open_error(99, "Error: Can't write to", argv[2]);
+
+	/* read from source and copy to dest file */
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		total_bytes += bytes_read;
+		if (write(fd_to, buffer, bytes_read) != bytes_read)
+			open_error(99, "Error: Can't write to", argv[2]);
+	}
+	/* detect error when reading from file */
+	if (total_bytes < 0)
+	{
+		close(fd_from);
+		close(fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 		exit(98);
-    }
-	fp_to = fopen(argv[2], "w");
-	if (fp_to == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n",
-			argv[2]);
-		exit(99);
 	}
 
-	while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, fp_from)) > 0)
-	{
-		bytes_written = fwrite(buffer, 1, bytes_read, fp_to);
-		if (bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n"
-				, argv[2]);
-			exit(99);
-		}
-	}
-	if (bytes_read < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]);
-		exit(98);
-	}
-	if (fclose(fp_from) == EOF || fclose(fp_to) == EOF)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		exit(100);
-	}
+	f_close = close(fd_from);
+	if (f_close < 0)
+		close_error(100, "Error: Can't close", fd_from);
+
+	f_close = close(fd_to);
+	if (f_close < 0)
+		close_error(100, "Error: Can't close", fd_to);
+
+	free(buffer);
 	return (0);
 }
