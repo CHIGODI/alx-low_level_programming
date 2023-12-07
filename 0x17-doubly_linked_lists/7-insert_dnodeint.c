6533d7ba1fc114f638b4c02c7644b36908802b41
CHIGODI <chiegody254@gmail.com>
2023-12-07 16:23:27 +0300

    Fix: correct output

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,22 +35,19 @@ size_t dlistint_len(const dlistint_t *h)
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp, *prev;
-	unsigned int index_check = 0, len = 0;
+	unsigned int index_check = 0;
+	size_t len = dlistint_len(*h);
 
-	if (*h == NULL && idx > 0)
+	if (idx > len)
 		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	len = dlistint_len(*h);
 	new_node->n = n;
-	if (idx > len)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	if (idx == 0)
+
+	if (idx == 0 || *h == NULL)
 	{
 		new_node->prev = NULL;
 		new_node->next = *h;
@@ -60,18 +57,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 	temp = *h;
-	while (temp != NULL)
+
+	for (i = 0; i < idx - 1; i++)
 	{
-		if (index_check == idx)
+		if (temp->next == NULL)
 		{
-			new_node->next = temp;
-			new_node->prev = prev;
-			temp->prev->next = new_node;
-			return (new_node);
+			free(new_node);
+			return (NULL);
 		}
-		prev = temp;
 		temp = temp->next;
-		index_check++;
 	}
-	return (NULL);
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return (new_node);
 }
