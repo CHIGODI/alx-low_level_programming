31eddf0c1407bdaa44292dece2a731eb1169cad7
CHIGODI <chiegody254@gmail.com>
2023-12-07 11:55:31 +0300

    Implement task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - prints all elements of a dlistint_t
+ * @h: pointer to first node
+ *
+ * Return: number of nodes
+ *
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (-1);
+
+	while (h != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
