afef6306da4e7bc6f5af5b5f77c4a67bb87f5083
CHIGODI <chiegody254@gmail.com>
2023-12-07 15:56:41 +0300

    Fix: correct output

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,24 +3,25 @@
 /**
  * dlistint_len - returns length of dlistint_t
  * @h: pointer to first node
-  *
+ *
  * Return: length of list
  *
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-        unsigned int count = 0;
+	unsigned int count = 0;
 
-        if (h == NULL)
-                return (0);
+	if (h == NULL)
+		return (0);
 
-        while (h != NULL)
-        {
-                count++;
-                h = h->next;
-        }
-        return (count);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
+
 /**
  * insert_dnodeint_at_index - inserts a new node at a
  *                            given position
@@ -34,19 +35,16 @@ size_t dlistint_len(const dlistint_t *h)
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp, *prev;
-	unsigned int index_check = 0;
-	unsigned int len = 0;
+	unsigned int index_check = 0, len = 0;
 
 	if (*h == NULL && idx > 0)
 		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	len = dlistint_len(*h);
 	new_node->n = n;
-
 	if (idx > len)
 	{
 		free(new_node);
@@ -61,7 +59,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-
 	temp = *h;
 	while (temp != NULL)
 	{
