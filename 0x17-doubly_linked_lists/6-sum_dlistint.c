3609574c9904bccfaa49d3e2b3ca3fa972239c08
CHIGODI <chiegody254@gmail.com>
2023-12-07 13:46:36 +0300

    Implement task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * sum_dlistint - finds sum of all the data of a dlistint
+ * @head: pointer to first node
+ *
+ * Return: Sum or 0 if list empty
+ *
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	int sum = 0;
+
+	if (head == NULLL)
+		return (0);
+
+	temp = head;
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
