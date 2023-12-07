d356ed8059123a12118ad91d547aa690d5ca3f32
CHIGODI <chiegody254@gmail.com>
2023-12-07 15:46:57 +0300

    Fix: correct output

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * dlistint_len - returns length of dlistint_t
  * @h: pointer to first node
- *
+  *
  * Return: length of list
  *
  */
@@ -37,6 +37,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int index_check = 0;
 	unsigned int len = 0;
 
+	if (*h == NULL && idx > 0)
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -44,11 +47,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	len = dlistint_len(*h);
 	new_node->n = n;
 
-	if (*h == NULL && idx > 0)
-		return (NULL);
-
 	if (idx > len)
+	{
+		free(new_node);
 		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
 	temp = *h;
 	while (temp != NULL)
