7bc14e20e38d20aa6aa8cf48ae5b6c9fdbc04495
CHIGODI <chiegody254@gmail.com>
2023-12-07 12:08:01 +0300

    Fix: betty and corrected check for null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,11 +12,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
+	if (head == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	if (head == NULL ||*head == NULL)
+	if (*head == NULL)
 	{
 		*head = new_node;
 		new_node->n = n;
