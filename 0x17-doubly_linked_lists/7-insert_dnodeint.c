02b2411eb66fb6caab61dfeb4470ceab1695bf72
CHIGODI <chiegody254@gmail.com>
2023-12-07 16:01:44 +0300

    Fix: correct output

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -66,7 +66,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			new_node->next = temp;
 			new_node->prev = prev;
-			prev->next = new_node;
+			temp->prev->next = new_node;
 			return (new_node);
 		}
 		prev = temp;
