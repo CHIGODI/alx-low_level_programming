9d402f932ac91d29a1f7191d1b9e1210ffb57fbd
CHIGODI <chiegody254@gmail.com>
2023-12-21 20:59:41 +0300

    Fix: compile errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,4 @@
-#incluse "has_tables.h"
+#include "hash_tables.h"
 
 /**
  * hash_table_set - adds an element to the hash table
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, size);
+	index = key_index((const unsigned char *)key, ht->size);
 
 	array_node = malloc(sizeof(hash_node_t));
 	if (array_node == NULL)
