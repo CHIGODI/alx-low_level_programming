3b9f6e03f986e7508f95eeaa2399ac27edb238c9
CHIGODI <chiegody254@gmail.com>
2023-12-21 20:27:48 +0300

    Fix: initialize table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned int i;
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
