02e77bb9217f93220f502810d4da3be3766ddd9b
CHIGODI <chiegody254@gmail.com>
2023-12-21 13:56:27 +0300

    Implement task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ *
+ * Return: alue associated with the element, or NULL if key
+ *         couldn’t be found
+ *
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hast_table_t *curr_node;
+	unsigned int index;
+
+	if (key == NULL || ht == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned int char *(key)), ht->size);
+	curr_node = ht->array[index];
+	while (curr_node != NULL)
+	{
+		if (strcmp(curr_node, key) == 0)
+			return (curr_node);
+
+		curr_node = curr_node->next;
+	}
+	return (NULL);
+}
