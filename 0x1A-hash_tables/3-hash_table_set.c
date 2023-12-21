153e6cc17fd9bbd67922fa2369d090a218523ac9
CHIGODI <chiegody254@gmail.com>
2023-12-21 20:57:41 +0300

    Fix: add string library to header

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #incluse "has_tables.h"
+
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: pointer to hash_table
@@ -11,12 +12,12 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index;
-	hash_node_t *array_node;
+	hash_node_t *array_node = NULL;
 
 	if (ht == NULL || key == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, size);
 
 	array_node = malloc(sizeof(hash_node_t));
 	if (array_node == NULL)
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,7 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include <string.h>
 #include <stdlib.h>
 #include <stdlib.h>
 /**
