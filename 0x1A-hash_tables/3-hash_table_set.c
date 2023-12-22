3fda95cb63692741349c56145fb01a78c65ba0b8
CHIGODI <chiegody254@gmail.com>
2023-12-22 20:11:31 +0300

    Fix: add case key exists

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,12 +13,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *array_node = NULL;
+	hash_node_t *curr = NULL;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
+	curr = ht->array[index];
+	/** if key already exists in list update it value/overwrite its value**/
+	while (curr != NULL)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			free(curr->value);
+			curr->value = strdup(value);
+			if (curr->value == NULL)
+				return (0);
+			return (1);
+		}
+		curr = curr->next;
+	}
 	array_node = malloc(sizeof(hash_node_t));
 	if (array_node == NULL)
 		return (0);
