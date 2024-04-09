aa3b7c7a8b6a5fbb3bb786aa81735e40ebfe8c77
CHIGODI <chiegody254@gmail.com>
2024-04-09 13:22:52 +0300

    Binary search edge case if size = 0

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,7 +11,7 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, middle;
+	size_t left = 0, right = size - 1, middle, i = 0;
 
 	if (size == 0)
 		return (-1);
