dd78aab547f3ca2cd9926eabe9c718ece5a738d2
CHIGODI <chiegody254@gmail.com>
2024-04-11 08:28:06 +0300

    Add jump search algorithim

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * jump_search - jump search algorithim
+ * @array: array to search
+ * @size: number of elements in array
+ * @value: value we are looking for
+ *
+ * Return:  index where value is located or -1 otherwise
+ *
+ *
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t jump, left, right, i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	jump = sqrt(size);
+	left = 0;
+	right = jump;
+
+	while (right < size && array[right] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", right,
+		       array[right]);
+		left = right;
+		right += jump;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
+
+	for (i = left; i <= (right < size ? right : size - 1); i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
